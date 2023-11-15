1c99710c3bb7181d40ee0c22460ac2103fbf11e4
Caleb Schilly <cwschilly@gmail.com>
2023-11-15 12:37:21 -0500

    add matrix-matrix multiplication test

--- tests/distributed/test_basic_A_transpose_dot_b_via_python.py
+++ tests/distributed/test_basic_A_transpose_dot_b_via_python.py
@@ -20,6 +20,21 @@ def distribute_array(global_array, comm):
 
     return local_array
 
+def run_At_dot_B(A, B, comm):
+    rank = comm.Get_rank()
+
+    A_dist = distribute_array(A, comm)
+    B_dist = distribute_array(B, comm)
+
+    dot_result = _basic_A_transpose_dot_b_via_python(A_dist, B_dist, comm)
+
+    if rank == 0:
+        dot_expected = np.dot(A.transpose(), B)
+        return dot_result, dot_expected
+
+    else:
+        return None, None
+
 def test_basic_A_transpose_dot_b_via_python_gram():
     '''Tests A^T A where A is row-distributed'''
     comm = MPI.COMM_WORLD
@@ -28,13 +43,26 @@ def test_basic_A_transpose_dot_b_via_python_gram():
 
     A = np.random.rand(num_processes*2, 3)
 
-    A_dist = distribute_array(A, comm)
-    dot_result = _basic_A_transpose_dot_b_via_python(A_dist, A_dist, comm)
+    gram_result, gram_expected = run_At_dot_B(A, A, comm) # A^T A
 
     if rank == 0:
-        dot_expected = np.dot(A.transpose(), A)
-        assert np.allclose(dot_result, dot_expected)
+        assert np.allclose(gram_result, gram_expected)
+
+def test_basic_A_transpose_dot_b_via_python_mat_mat():
+    '''Tests A^T B where A and B are different row-distributed matrices'''
+    comm = MPI.COMM_WORLD
+    num_processes = comm.Get_size()
+    rank = comm.Get_rank()
+
+    A = np.random.rand(num_processes*2, 3)
+    B = np.random.rand(num_processes*2, 3)
+
+    mat_mat_result, mat_mat_expected = run_At_dot_B(A, A, comm) # A^T B
+
+    if rank == 0:
+        assert np.allclose(mat_mat_result, mat_mat_expected)
 
 
 if __name__ == "__main__":
     test_basic_A_transpose_dot_b_via_python_gram()
+    test_basic_A_transpose_dot_b_via_python_mat_mat()
