aba9174a90763c878d7adb6f75a187a5955fb7b8
Caleb Schilly <cwschilly@gmail.com>
2023-11-17 15:38:29 -0500

    add binding tests to pytest

--- tests/test_bindings.py
+++ tests/test_bindings.py
@@ -6,17 +6,22 @@ import mpi4py
 from pressiolinalg.linalg import *
 
 # TESTING MYFUNC
-print("Testing myfunc")
-vector = np.arange(1,10)
-func = myfunc(vector)
+def test_myfunc():
+    vector = np.arange(1,10)
+    func = myfunc(vector)
+    assert 2 == 2 # this test is temporary
 
 # TESTING PRINT_COMM
-print("\nTesting print_comm")
-comm = MPI.COMM_WORLD
-comm_address = hex(MPI._addressof(comm))
-ftn_address = hex(print_comm(comm))
+def test_print_comm:
+    comm = MPI.COMM_WORLD
+    comm_address = hex(MPI._addressof(comm))
+    ftn_address = hex(print_comm(comm))
 
-print(f"    comm address:     {comm_address}")
-print(f"    function address: {ftn_address}")
+    print(f"    comm address:     {comm_address}")
+    print(f"    function address: {ftn_address}")
 
-assert ftn_address == comm_address, f"{ftn_address} != {comm_address}"
+    assert ftn_address == comm_address, f"{ftn_address} != {comm_address}"
+
+if __name__ == "__main__":
+    test_myfunc()
+    test_print_comm()
