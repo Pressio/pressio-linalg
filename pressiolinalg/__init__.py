a8bddcc74b6d1b8bbf25365c5d018465bf4dbfb9
Caleb Schilly <cwschilly@gmail.com>
2024-03-04 12:49:59 -0500

    #18: improve version call

--- pressiolinalg/__init__.py
+++ pressiolinalg/__init__.py
@@ -2,9 +2,5 @@
 import os
 
 topdir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-def get_version():
-  with open(os.path.join(topdir, "version.txt")) as f:
-    return f.read()
-
-__version__ = get_version()
+with open(os.path.join(topdir, "version.txt")) as f:
+    __version__ = f.read().strip()
